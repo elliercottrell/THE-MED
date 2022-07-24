712684d29691adfcb4dec5599108a13e0716b1c5
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-07-24 19:01:30 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const circleProgress = document.querySelector(".circle-progress");
+const numberOfBreaths = document.querySelector(".breath-input");
+const start = document.querySelector(".start");
+const instructions = document.querySelector(".instructions");
+const breathsText = document.querySelector(".breaths-text");
+let breathsLeft = 3;
+
+//listening for selected breaths from user
+numberOfBreaths.addEventListener("change", () => {
+  breathsLeft = numberOfBreaths.value;
+  breathsText.innerText = breathsLeft;
+});
+
+// grow/shrink circle
+const growCircle = () => {
+  circleProgress.classList.add("circle-grow");
+  setTimeout(() => {
+    circleProgress.classList.remove("circle-grow");
+  }, 8000);
+};
+
+// breathing instructions
+const breathTextUpdate = () => {
+  breathsLeft--;
+  breathsText.innerText = breathsLeft;
+  instructions.innerText = "Inhale";
+  setTimeout(() => {
+    instructions.innerText = "Hold";
+    setTimeout(() => {
+      instructions.innerText = "Exhale";
+    }, 4000);
+  }, 4000);
+};
+
+// breathing App function
+const breathingApp = () => {
+  const breathingAnimation = setInterval(() => {
+    if (breathsLeft === 0) {
+      clearInterval(breathingAnimation);
+      instructions.innerText = "session complete";
+      start.classList.remove("button-inactive");
+      breathsLeft = numberOfBreaths.value;
+      breathsText.innerText = breathsLeft;
+      return;
+    }
+    growCircle();
+    breathTextUpdate();
+  }, 12000);
+};
+
+// start breathing
+start.addEventListener("click", () => {
+  start.classList.add("button-inactive");
+  instructions.innerText = "ok ";
+  setTimeout(() => {
+    instructions.innerText = "2 seconds ";
+    setTimeout(() => {
+      breathingApp();
+      growCircle();
+      breathTextUpdate();
+    }, 2000);
+  }, 2000);
+});
